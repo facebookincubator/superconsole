9c33b815046fa5884fedad7df3f119df9c87e4ae
Thomas Orozco <torozco@fb.com>
2022-03-15 07:20:24 -0700

    buck2: superconsole: add more output hooks
    Summary: Like it says in the title. I'm using those later in the stack.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D34818036
    
    fbshipit-source-id: 1697ccd899b89582472e7c18c7ee332304b6ca6b

--- src/output.rs
+++ src/output.rs
@@ -10,19 +10,35 @@
 use std::io::{self, Write};
 
 pub trait SuperConsoleOutput: Send + Sync + 'static {
+    /// Called before rendering will occur. This has a chance to prevent rendering by returning
+    /// false.
+    fn should_render(&mut self) -> bool;
+
     /// Called to produce output. This may be called without pre_render if we are finalizing. This
     /// should flush if possible.
     fn output(&mut self, buffer: Vec<u8>) -> anyhow::Result<()>;
+
+    /// Called when the console has finalized. This must block if necessary. No further output will
+    /// be emitted.
+    fn finalize(&mut self) -> anyhow::Result<()>;
 }
 
 pub(crate) struct StderrSuperConsoleOutput;
 
 impl SuperConsoleOutput for StderrSuperConsoleOutput {
+    fn should_render(&mut self) -> bool {
+        true
+    }
+
     fn output(&mut self, buffer: Vec<u8>) -> anyhow::Result<()> {
         let stderr = io::stderr();
         let mut handle = stderr.lock();
         handle.write_all(&buffer)?;
         handle.flush()?;
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> anyhow::Result<()> {
         Ok(())
     }
 }
